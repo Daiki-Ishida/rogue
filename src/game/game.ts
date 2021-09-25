a5b7d8550b905fa9d246af58b0eaff4a1ec08d2e
Daiki-Ishida <ishidadaiki@team-lab.com>
2021-09-25 15:19:29 +0900

    [add]game class追加

---
+++ src/game/game.ts
@@ -0,0 +1,28 @@
+import { Board } from './board';
+import { Commands } from './command';
+import { Inventory } from './inventory';
+import { Player } from './models/actor';
+import { Turn } from './turn';
+
+export class Game {
+  constructor(
+    readonly player: Player,
+    readonly board: Board,
+    readonly commands: Commands,
+    readonly turn: Turn,
+    readonly inventory: Inventory
+  ) {}
+
+  static init(): Game {
+    const player = Player.init('Player');
+    const board = Board.init(60, 30);
+    const commands = Commands.init();
+    const turn = Turn.init();
+    const inventory = Inventory.init();
+    return new Game(player, board, commands, turn, inventory);
+  }
+
+  proc(): void {
+    this.turn.proc(this);
+  }
+}
--- src/game/index.ts
+++ src/game/index.ts
@@ -1,27 +1,66 @@
-import p5 from 'p5';
+import p5, { Graphics } from 'p5';
+import { Asset } from 'asset';
+import { Game } from './game';
+import { ImageStore } from './store';
+import { Drawer } from './view';
+import { Controller } from './controller';
+import { WindowManager } from './view/gui';
+import { AnimationManager } from './animation';
+
+let asset: Asset;
+let controller: Controller;
+export let overlay: Graphics;
+export let game: Game;
+export let drawer: Drawer;
+export let imageStore: ImageStore;
+export let windowManager: WindowManager;
+export let animationManager: AnimationManager;
 
 export const rogue = (p: p5): void => {
   p.preload = () => {
     // todo
-    return;
+    asset = Asset.preload(p);
+    imageStore = ImageStore.init(asset);
+
+    overlay = p.createGraphics(1280, 720);
   };
 
   p.setup = () => {
     p.frameRate(30);
     p.createCanvas(1280, 720);
+    p.textFont(asset.font);
+
+    game = Game.init();
+    drawer = Drawer.init();
+    controller = Controller.init();
+
+    animationManager = AnimationManager.init();
+    windowManager = WindowManager.init(game);
   };
 
   p.keyPressed = () => {
-    // todo
-    return;
+    if (moveKeyIsDown(game, p)) {
+      return;
+    }
+
+    controller.proc(p.key, game);
   };
 
   p.draw = () => {
-    if (p.mouseIsPressed) {
-      p.fill(0);
-    } else {
-      p.fill(255);
-    }
-    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+    drawer.draw(game, p);
   };
+};
+
+const moveKeyIsDown = (game: Game, p: p5) => {
+  // if (game.inventory.display) return false;
+  // if (game.selectWindow) return false;
+  // if (game.potContentsWindow) return false;
+
+  return (
+    p.key !== 'A' &&
+    p.key !== 'W' &&
+    p.key !== 'S' &&
+    p.key !== 'D' &&
+    (p.keyIsDown(65) || p.keyIsDown(87) || p.keyIsDown(83) || p.keyIsDown(68))
+  );
 };
